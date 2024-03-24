6528dd4832c5f0c53eea51384966ca69929bd5b3
Tushar Jain <tusharlock10@gmail.com>
2024-03-25 01:40:30 +0530

    feat: add client disconnects and reconnects

--- server/main.go
+++ server/main.go
@@ -1,95 +1,16 @@
 package main
 
 import (
-	"context"
+	"chatapp/services"
 	"fmt"
 	"net/http"
-	"sync"
-
-	"time"
-
-	"github.com/google/uuid"
-	"nhooyr.io/websocket"
-	"nhooyr.io/websocket/wsjson"
 )
 
-// store all the connected clients
-var clients sync.Map
-
-type Message struct {
-	UUID      string    `json:"uuid"`
-	Username  string    `json:"text"`
-	Text      string    `json:"text"`
-	CreatedAt time.Time `json:"createdAt"`
-}
-
-func connect(w http.ResponseWriter, r *http.Request) {
-	conn, err := websocket.Accept(w, r, nil)
-	if err != nil {
-		fmt.Printf("Error accepting connection: %v\n", err)
-		return
-	}
-	// once the socket is made, add the client to the clients map
-	clients.Store(conn, true)
-	clientIp := r.RemoteAddr
-	fmt.Printf("New client connected : %v\n", clientIp)
-
-	// launch a goroutine to handle this client
-	go handleClient(conn)
-}
-
-func handleClient(conn *websocket.Conn) {
-	defer disconnectClient(conn)
-
-	// continuously read messages from this connection
-	for {
-		var msg Message
-
-		// message will contain the text and sender initially
-		err := wsjson.Read(context.Background(), conn, &msg)
-		if err != nil {
-			fmt.Printf("Error reading message: %v\n", err)
-			break
-		}
-
-		// add additional fields in the message
-		msg.CreatedAt = time.Now()
-		msg.UUID = uuid.New().String()
-
-		fmt.Printf("Received message: %s from %s\n", msg.Text, msg.CreatedAt.Format(time.RFC1123))
-		broadcast(&msg)
-	}
-}
-
-func disconnectClient(conn *websocket.Conn) {
-	// disconnects the client gracefully
-	clients.Delete(conn)
-	conn.Close(websocket.StatusNormalClosure, "Client disconnected")
-}
-
-func broadcast(msg *Message) {
-	// send message to all clients, even the sender of the message
-	clients.Range(func(key, value any) bool {
-		client, ok := key.(*websocket.Conn)
-		if !ok {
-			return true // continue iteration
-		}
-
-		err := wsjson.Write(context.Background(), client, msg)
-		if err != nil {
-			fmt.Printf("Error broadcasting message: %v\n", err)
-			return false
-		}
-		return true
-	})
-}
-
 func main() {
-	http.HandleFunc("/chat", connect)
+	http.HandleFunc("/chat", services.Chat)
 
 	fmt.Println("Server started on :8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Printf("Error starting server: %v\n", err)
 	}
 }
---
+++ server/services/chat.go
@@ -0,0 +1,99 @@
+package services
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+	"nhooyr.io/websocket"
+	"nhooyr.io/websocket/wsjson"
+)
+
+// Map  of clients that are currently connected
+var clients sync.Map
+
+// Message contains the text contents and username of the sender
+type Message struct {
+	UUID      string
+	Username  string
+	CreatedAt time.Time
+	Text      string `json:"text"`
+}
+
+// A client is the username with their connection
+type Client struct {
+	Username string
+	Conn     *websocket.Conn
+}
+
+// Function to start connections and handle them
+func Chat(w http.ResponseWriter, r *http.Request) {
+	conn, err := websocket.Accept(w, r, nil)
+	if err != nil {
+		fmt.Printf("Error accepting connection: %v\n", err)
+		return
+	}
+
+	go handleConnection(r, conn)
+}
+
+func handleConnection(r *http.Request, conn *websocket.Conn) {
+	// require a username to send messages
+	username := r.URL.Query().Get("username")
+	if username == "" {
+		conn.Close(websocket.StatusPolicyViolation, "Username is required")
+		return
+	}
+
+	if value, ok := clients.Load(username); ok {
+		existingClient := value.(*Client)
+		fmt.Printf("Disconnecting previous connection for user: %s\n", username)
+
+		// close the old connection before using the new one
+		existingClient.Conn.Close(websocket.StatusPolicyViolation, "Another connection established")
+		fmt.Printf("Client reconnected: %s\n", username)
+	} else {
+		fmt.Printf("Client connected: %s\n", username)
+	}
+
+	// store the new connection
+	client := &Client{Username: username, Conn: conn}
+	clients.Store(username, client)
+
+	handleClient(client)
+}
+
+// Listens to messages from a client and broadcasts them to all clients
+func handleClient(client *Client) {
+	for {
+		var msg Message
+		err := wsjson.Read(context.Background(), client.Conn, &msg)
+		if err != nil {
+			// disconnect the client if incorrect data is received
+			clients.Delete(client.Username)
+			client.Conn.Close(websocket.StatusUnsupportedData, "Unable to parse data")
+			break
+		}
+
+		msg.CreatedAt = time.Now()
+		msg.UUID = uuid.New().String()
+		msg.Username = client.Username
+
+		fmt.Printf("Received message: %s - by %s\n", msg.Text, msg.Username)
+		broadcast(&msg)
+	}
+}
+
+// Send a message to all connected clients
+func broadcast(msg *Message) {
+	clients.Range(func(key, value any) bool {
+		client, ok := value.(*Client)
+		if ok {
+			wsjson.Write(context.Background(), client.Conn, msg)
+		}
+		return true
+	})
+}
