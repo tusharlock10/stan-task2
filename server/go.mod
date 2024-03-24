271aff6d0ba67b5ef5d3e8a20f88f79bac602568
Tushar Jain <tusharlock10@gmail.com>
2024-03-24 23:31:28 +0530

    feat: add ws connections server

--- server/go.mod
+++ server/go.mod
@@ -3,3 +3,5 @@ module chatapp
 go 1.22.1
 
 require nhooyr.io/websocket v1.8.10
+
+require github.com/google/uuid v1.6.0
--- server/main.go
+++ server/main.go
@@ -6,12 +6,22 @@ import (
 	"net/http"
 	"sync"
 
+	"time"
+
+	"github.com/google/uuid"
 	"nhooyr.io/websocket"
 	"nhooyr.io/websocket/wsjson"
 )
 
 // store all the connected clients
 var clients sync.Map
+
+type Message struct {
+	UUID      string    `json:"uuid"`
+	Username  string    `json:"text"`
+	Text      string    `json:"text"`
+	CreatedAt time.Time `json:"createdAt"`
+}
 
 func connect(w http.ResponseWriter, r *http.Request) {
 	conn, err := websocket.Accept(w, r, nil)
@@ -22,38 +32,43 @@ func connect(w http.ResponseWriter, r *http.Request) {
 	// once the socket is made, add the client to the clients map
 	clients.Store(conn, true)
 	clientIp := r.RemoteAddr
-	fmt.Printf("New client connected : %v", clientIp)
+	fmt.Printf("New client connected : %v\n", clientIp)
 
 	// launch a goroutine to handle this client
 	go handleClient(conn)
 }
 
 func handleClient(conn *websocket.Conn) {
-	// handles the client connection
 	defer disconnectClient(conn)
 
 	// continuously read messages from this connection
 	for {
-		var msg interface{}
+		var msg Message
+
+		// message will contain the text and sender initially
 		err := wsjson.Read(context.Background(), conn, &msg)
 		if err != nil {
 			fmt.Printf("Error reading message: %v\n", err)
 			break
 		}
 
-		fmt.Printf("Received message: %s\n", msg)
+		// add additional fields in the message
+		msg.CreatedAt = time.Now()
+		msg.UUID = uuid.New().String()
+
+		fmt.Printf("Received message: %s from %s\n", msg.Text, msg.CreatedAt.Format(time.RFC1123))
 		broadcast(&msg)
 	}
 }
 
 func disconnectClient(conn *websocket.Conn) {
-	// disconnects the client
+	// disconnects the client gracefully
 	clients.Delete(conn)
 	conn.Close(websocket.StatusNormalClosure, "Client disconnected")
 }
 
-func broadcast(msg *interface{}) {
-	// Iterate over all clients and send the message
+func broadcast(msg *Message) {
+	// send message to all clients, even the sender of the message
 	clients.Range(func(key, value any) bool {
 		client, ok := key.(*websocket.Conn)
 		if !ok {
